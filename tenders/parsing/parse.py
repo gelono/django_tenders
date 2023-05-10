acbba6be947936308bad81b29061ca363e1db3d8
gelono <gelonooz@gmail.com>
2023-05-10 14:12:49 +0300

    add Dockerfile, docker-compose.yml

--- tenders/parsing/parse.py
+++ tenders/parsing/parse.py
@@ -1,11 +1,17 @@
 from threading import Thread
 
+from celery import shared_task
+
 from tenders.parsing.parsing import manager_for_collecting_links, manager_for_processing_active_tenders
 
 
+@shared_task()
 def start_parse_prozorro():
     p1 = Thread(target=manager_for_collecting_links, args=(5, ))
     p2 = Thread(target=manager_for_processing_active_tenders, args=(5,))
 
     p1.start()
     p2.start()
+
+
+# start_parse_prozorro()
